0d7e93105c9cd120997b56811018ea709c871e5c
Gabriela Garcia <g.garcias2@uniandes.edu.co>
2023-11-29 15:07:10 -0500

    conuslta basica y script de inserciuon de datpos

---
+++ docs/it3/script.py
@@ -0,0 +1,211 @@
+from faker import Faker
+from faker.providers import DynamicProvider
+from datetime import datetime
+from datetime import datetime, timedelta
+from datetime import datetime, timezone
+
+import random
+
+fake = Faker('es_CO')
+fecha_inicial = datetime(1990, 1, 1)
+
+# Definir la fecha final (límite hasta el año 2024)
+fecha_final = datetime(2024, 12, 31)
+reservas=[]
+consumos=[]
+habitaciones=[]
+
+class Tiposhab:
+    def __init__(self, id):  # Fixed typo here (__init__ instead of _init_)
+        self.tipo= fake.random_element(elements=['suite', 'suite presidencial', 'familiar', 'doble','sencilla'])
+        self.descripcion = fake.text(max_nb_chars=50)
+        self.id = id
+
+    def sql_add(self):
+        return f"INSERT INTO tiposhab (tipo, descripcion, id) VALUES ('{self.tipo}', '{self.descripcion}', {self.id});"
+
+class Habitacion:
+    def __init__(self, id):  # Fixed typo here (__init__ instead of _init_)
+        self.id = id
+        self.capacidad = fake.random_int(min=1, max=8, step=1)
+        self.tipoid = fake.random_int(min=1, max=5, step=1)
+
+    def sql_add(self):
+        return f"INSERT INTO habitacion (id, capacidad, tipoid) VALUES ({self.id}, {self.capacidad}, {self.tipoid});"
+
+"""
+fecha_inicial = datetime(2022, 1, 1)
+
+# Definir la fecha final (límite hasta el año 2024)
+fecha_final = datetime(2024, 12, 31)
+
+# Generar una fecha aleatoria entre la fecha inicial y la fecha final
+fecha_aleatoria = fecha_inicial + (fecha_final - fecha_inicial) * random.random()
+"""
+class Reservas:
+    
+    def __init__(self):  # Fixed typo here (__init__ instead of _init_)
+        fechaentrada = fecha_inicial + (fecha_final - fecha_inicial) * random.random()
+
+# Suponiendo que fecha_final es la última fecha posible (ej. 31 de diciembre del mismo año)
+        fin_de_año = datetime(fechaentrada.year, 12, 31)
+
+# Generar una fecha aleatoria entre fechaentrada y fin_de_año.
+        if (fin_de_año - fechaentrada).days > 0:
+            dias_random = random.randint(1, (fin_de_año - fechaentrada).days)
+        else:
+            # Si no hay diferencia de días, usa el mismo día
+            dias_random = 0
+        fechasalida = fechaentrada + timedelta(days=dias_random)
+  
+      
+        self.fechaentrada = fechaentrada.strftime('%Y-%m-%dT%H:%M:%SZ')
+        self.fechasalida = fechasalida.strftime('%Y-%m-%dT%H:%M:%SZ')
+        self.numpersonas = fake.random_int(min=1, max=6, step=1)
+        self.estado = fake.random_element(elements=['true', 'false'])
+        precio_entero = random.randint(1000, 1000000)
+        decimales = random.randint(1, 99)
+        self.precio=float(f"{precio_entero}.{decimales:02d}")
+        self.doc=fake.numerify(text='#########')
+        
+        
+        #self.habitacionid = habitaciones[fake.random_int(min=0, max=len(habitaciones), step=1)]
+        
+   
+         
+   
+    
+    def sql_add(self):
+        #return '{'+ f"'fechaentrada':  new Date('{self.fechaentrada}'), 'fechasalida': , 'numpersonas': , 'estado': , 'precioreserva': , 'docUsuario' :  ,'habitacionId':", "} "
+        return f"'fechaentrada': new Date('{self.fechaentrada}'), 'fechasalida': new Date('{self.fechasalida}'), 'numpersonas': {self.numpersonas} , 'estado':{self.estado} , 'precioreserva': {self.precio}, 'docUsuario' : '{self.doc}' " #,'habitacionId': "
+    
+    
+
+class Consumos:
+    def __init__(self, id):
+  
+        self.id=id
+        fecha=fecha_inicial + (fecha_final - fecha_inicial) * random.random() 
+        self.fecha=fecha.replace(microsecond=0)
+        
+    def getFechaConsumoPorID(self):
+        return self.fecha
+        
+    def sql_add(self):
+        return f"INSERT INTO consumos (id, fecha ) VALUES ({self.id}, TO_DATE('{self.fecha}', 'YYYY-MM-DD HH24:MI:SS'));"
+        
+
+class Producto:
+    def __init__(self, id):
+        self.nombre = fake.word()
+        self.consumoid = fake.random_int(min=1, max=100, step=1)
+        self.id = id
+        self.tiendaid = fake.random_int(min=1, max=100, step=1)
+        self.smercadoid = fake.random_int(min=1, max=100, step=1)
+        self.preciofinal = random.randint(1000,1000000)
+    def sql_add(self):
+        return f"INSERT INTO productos (nombre, consumoid, id, tiendaid, smercadoid, preciofinal ) VALUES ('{self.nombre}', {self.consumoid}, {self.id}, {self.tiendaid}, {self.smercadoid}, {self.preciofinal});"
+class Servicio:
+    def __init__(self, id):
+        self.id = id
+        self.descripcion = fake.random_element(elements=['Piscina', 'Gimnasio', 'Internet', 'Lavado, planchado','Restaurante', 'Bar', 'Super Mercado', 'Tiendas', 'SPA', 'Utensilios', 'Salon de reuniones', 'Salon de conferencias'])
+        self.precio = random.randint(1000,1000000)
+        self.consumoid = fake.random_int(min=1, max=46000, step=1)
+    def sql_add(self):
+        return f"INSERT INTO servicio (id, descripcion, precio, consumoid) VALUES ({self.id}, '{self.descripcion}', {self.precio}, {self.consumoid});"
+class Tienda:
+    def __init__(self, id):
+        self.nombre = fake.text(max_nb_chars=15)
+        self.horarioapertura = self.generar_hora_inicial()
+        self.horariocierre = self.generar_hora_final()
+        self.capacidad = random.randint(20,150)
+        self.id = id
+    def generar_hora_inicial(self):
+        hora_aleatoria = random.randint(0, 12)
+        minutos_aleatorios = random.randint(0, 59)
+        return f"{hora_aleatoria:02d}:{minutos_aleatorios:02d}" 
+    def generar_hora_final(self):
+        hora_aleatoria = random.randint(13, 23)
+        minutos_aleatorios = random.randint(0, 59)
+        return f"{hora_aleatoria:02d}:{minutos_aleatorios:02d}" 
+    def sql_add(self):
+        return f"INSERT INTO tiendas (nombre, horarioapertura, horariocierre, capacidad, id) VALUES ('{self.nombre}', TO_DATE('{self.horarioapertura}', 'HH24:MI'), TO_DATE('{self.horariocierre}', 'HH24:MI'), {self.capacidad}, {self.id});"
+class Mercado:
+    def __init__(self, id):
+        self.nombre = fake.text(max_nb_chars=15)
+        self.horarioapertura = self.generar_hora_inicial()
+        self.horariocierre = self.generar_hora_final()
+        self.capacidad = random.randint(20,150)
+        self.id = id
+    def generar_hora_inicial(self):
+        hora_aleatoria = random.randint(0, 12)
+        minutos_aleatorios = random.randint(0, 59)
+        return f"{hora_aleatoria:02d}:{minutos_aleatorios:02d}" 
+    def generar_hora_final(self):
+        hora_aleatoria = random.randint(13, 23)
+        minutos_aleatorios = random.randint(0, 59)
+        return f"{hora_aleatoria:02d}:{minutos_aleatorios:02d}" 
+    def sql_add(self):
+        return f"INSERT INTO smercados (nombre, horarioapertura, horariocierre, capacidad, id) VALUES ('{self.nombre}', TO_DATE('{self.horarioapertura}', 'HH24:MI'), TO_DATE('{self.horariocierre}', 'HH24:MI'), {self.capacidad}, {self.id});"
+
+
+
+        
+# Function to generate fake data and print SQL insert statements
+def populate(n):
+    for i in range(1, n + 1):
+        x = Habitacion(i)
+        print(x.sql_add())
+    
+
+    
+    print("db.reservas.insertMany([ ")
+ 
+       
+    for i in range(n):
+        if i<n-2:
+            x = Reservas()
+            reservas.append(x)
+            print("{", x.sql_add(), "},")
+        elif i==n-1:
+            x = Reservas()
+            reservas.append(x)
+            print("{", x.sql_add(), "}")
+        
+    print("])")
+       
+    for i in range(1, n + 1):
+        x = Consumos(i)
+        consumos.append(x)
+        print(x.sql_add())
+    
+    for i in range(1, n + 1):
+        x = Producto(i)
+        print(x.sql_add())
+    for i in range(1, n + 1):
+        x = Servicio(i)
+        print(x.sql_add())
+    for i in range(1, n + 1):
+        x = Tienda(i)
+        print(x.sql_add())
+    for i in range(1, n + 1):
+        x = Mercado(i)
+    
+        print(x.sql_add())
+   
+   
+
+      
+def populateTipos(n):
+    for i in range(1, n + 1):
+        x = Tiposhab(i)
+        print(x.sql_add())
+    
+
+    
+    
+        
+
+# Call the populate function to generate data for 10 users
+
+populate(10)
--- src/main/java/uniandes/edu/co/proyecto/controller/ReservaController.java
+++ src/main/java/uniandes/edu/co/proyecto/controller/ReservaController.java
@@ -1,17 +1,21 @@
 package uniandes.edu.co.proyecto.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.LookupOperation;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
 
 import uniandes.edu.co.proyecto.Modelo.Reserva;
 
@@ -24,6 +28,9 @@ public class ReservaController {
 
     @Autowired
     private ReservaRepository reservaRepository;
+
+    @Autowired
+private MongoTemplate mongoTemplate;
     
     //@Autowired
     //private HabitacionRepository  habitacionRepository;
@@ -81,6 +88,30 @@ public class ReservaController {
             reservaRepository.deleteById(id);
         }
         return "redirect:/reservas";
+    }
+
+
+    @GetMapping("/usuarios/RFC3")
+    public String RFC3(Model model, String id, String fecha1, String fecha2) {
+        LookupOperation lookupOperation = LookupOperation.newLookup()
+            .from("reservas")
+            .localField("idReserva")
+            .foreignField("_id")
+            .as("reservas");
+
+        Criteria idCriteria = Criteria.where("reservas.docUsuario").is(id);
+        Criteria fechaCriteria = Criteria.where("fecha").gte(fecha1).lte(fecha2);
+        Aggregation aggregation = Aggregation.newAggregation(
+            lookupOperation,
+            Aggregation.match(idCriteria.andOperator(fechaCriteria))
+        );
+        
+        
+
+       List<Reserva> reservas = mongoTemplate.aggregate(aggregation, "nombreDeTuColeccion", Reserva.class).getMappedResults();
+        model.addAttribute("consumosXFecha", reservas);
+        
+        return "usuarioreq5";
     }
 
 
