4bfce90b6a2d5f75c7d79aa2c83e3821b44b0448
Francois Morales <f.moraless@uniandes.edu.co>
2023-11-30 23:13:25 -0500

    Clase embebida platos y bebidas funcional :)

--- src/main/java/uniandes/edu/co/proyecto/controller/BarController.java
+++ src/main/java/uniandes/edu/co/proyecto/controller/BarController.java
@@ -1,5 +1,8 @@
 package uniandes.edu.co.proyecto.controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +12,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import uniandes.edu.co.proyecto.Modelo.Bar;
+import uniandes.edu.co.proyecto.Modelo.PlatosYBebidasEmbedded;
 import uniandes.edu.co.proyecto.repositorio.BarRepository;
 
 @Controller
@@ -29,12 +34,6 @@ public class BarController {
     public String barForm(Model model){
         model.addAttribute("bar", new Bar());
         return "barNuevo";       
-    }
-
-    @PostMapping("/bares/new/save")
-    public String barGuardar(@ModelAttribute Bar bar){
-        barRepository.save(bar);
-        return "redirect:/bares";
     }
 
     @GetMapping("/bares/{id}/edit")
@@ -63,4 +62,51 @@ public class BarController {
         return "redirect:/bares";
     }
     
+    //Controladores para el manejo de las bebidas y platos embebidos
+
+    @PostMapping("/bares/new/save")
+    public String crearBarBebida(@ModelAttribute("bar") Bar bar) {
+
+        // Guardamos el nuevo tipo de bebida
+        barRepository.save(bar);
+        return "redirect:/bares";
+    }
+
+    @GetMapping("/addBebida")
+    public String añadirBebida(@RequestParam(name = "nombre", required = false) String nombre, Model model){
+        model.addAttribute("nombreTipoBebida", nombre);
+        model.addAttribute("bebida", new PlatosYBebidasEmbedded());
+
+
+        return "addBebidaForm";
+    }
+
+    @PostMapping("/addBebidaSave")
+    public String añadirBebidaSave(@RequestParam("nombreTipoBebida") String nombreTipoBebida,
+    @ModelAttribute("bebida") PlatosYBebidasEmbedded beb){
+
+        // Creamos una nueva bebida utilizando los datos del formulario
+        PlatosYBebidasEmbedded nuevaBebida = new PlatosYBebidasEmbedded(
+            beb.getNombre(),
+            beb.getDescripcion()
+        );
+
+        //Buscamos los tipos de bebida con ese nombre
+        List<Bar> bebs = barRepository.findByNombre(nombreTipoBebida);
+
+        //Añadimos esa bebida a todos los tipos de bebidas con ese nombre
+        for (Bar tipoBebida:bebs){
+            if (tipoBebida.getPlatosybebidas() == null){
+                List<PlatosYBebidasEmbedded> emptyList = new ArrayList<>();
+                tipoBebida.setPlatosybebidas(emptyList);
+            }
+            tipoBebida.addPlatoYBebida(nuevaBebida);
+
+            //Persistemos la modificacion en la base de datos
+            barRepository.save(tipoBebida);
+        }
+        
+        return "redirect:/bares";
+
+    }
 }
